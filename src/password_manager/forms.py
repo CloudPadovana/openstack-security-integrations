499dd8263d5de132a18df9739d3377c00dd55a47
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2014-02-26 11:25:17 +0100

    Workaround for activate pwd

--- src/password_manager/forms.py
+++ src/password_manager/forms.py
@@ -1,7 +1,8 @@
-from django.conf import settings
+#from django.conf import settings
 from django.forms import ValidationError
 from django import http
 from django.utils.translation import ugettext_lazy as _
+from django.core.urlresolvers import reverse_lazy
 from django.views.decorators.debug import sensitive_variables
 
 from horizon import exceptions
@@ -23,32 +24,36 @@ class PasswordForm(forms.SelfHandlingForm):
                             widget=forms.PasswordInput(render_value=False))
 
     def clean(self):
-        '''Check to make sure password fields match.'''
         data = super(forms.Form, self).clean()
         if 'new_password' in data:
             if data['new_password'] != data.get('confirm_password', None):
                 raise ValidationError(_('Passwords do not match.'))
         return data
 
-    # We have to protect the entire "data" dict because it contains the
-    # oldpassword and newpassword strings.
     @sensitive_variables('data')
     def handle(self, request, data):
-        user_is_editable = api.keystone.keystone_can_edit_user()
 
-        if user_is_editable:
-            try:
-                api.keystone.user_update_own_password(request, None, data['new_password'])
-                response = http.HttpResponseRedirect(settings.LOGOUT_URL)
+        user_is_editable = api.keystone.keystone_can_edit_user()
+        if not user_is_editable:
+            messages.error(request, _('Activating password is not supported.'))
+            return False
+
+        try:
+            api.keystone.user_update_own_password(request, None, data['new_password'])
+            
+            if 'REMOTE_USER' in request.META and request.path.startswith('/dashboard-shib'):
+                #
+                # TODO verify workaround
+                #
+                return http.HttpResponseRedirect(reverse_lazy('login'))
+            else:
+                response = http.HttpResponseRedirect(reverse_lazy('logout'))
                 msg = _("Password changed. Please log in again to continue.")
                 utils.add_logout_reason(request, response, msg)
                 return response
-            except Exception:
-                exceptions.handle(request, _('Unable to change password.'))
-                return False
-        else:
-            messages.error(request, _('Changing password is not supported.'))
-            return False
+                
+        except Exception:
+            exceptions.handle(request, _('Unable to activate password.'))
 
-        return True
+        return False
 
