307a61e589531ab6fb4b68927e32b018235004c2
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2014-03-24 11:04:13 +0100

    Increased age number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.command.bdist_rpm import bdist_rpm as _bdist_rpm
 
 pkg_name = 'openstack-security-integrations'
 pkg_version = '1.0.0'
-pkg_release = '2'
+pkg_release = '3'
 
 source_items = "setup.py src"
 
