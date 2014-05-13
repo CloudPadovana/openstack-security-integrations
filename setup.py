b455e8b888a3f0088ca2eac88ef65d6e08c774fd
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2014-05-13 11:20:05 +0200

    Changed version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.command.bdist_rpm import bdist_rpm as _bdist_rpm
 
 pkg_name = 'openstack-security-integrations'
 pkg_version = '1.0.0'
-pkg_release = '4'
+pkg_release = '6'
 
 source_items = "setup.py src"
 
