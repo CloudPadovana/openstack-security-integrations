89c45370716f846bc49c4d8e7af597df405f249a
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2014-06-19 09:19:33 +0200

    Fixed duplicated subscription requests

--- src/project_requests/forms.py
+++ src/project_requests/forms.py
@@ -12,7 +12,10 @@ from openstack_auth_shib.models import Project
 from openstack_auth_shib.models import PrjRequest
 
 from openstack_auth_shib.models import PRJ_PUBLIC,PRJ_PRIVATE
-from openstack_auth_shib.models import OS_SNAME_LEN 
+from openstack_auth_shib.models import OS_SNAME_LEN
+
+from openstack_auth_shib.notifications import NotificationMessage
+from openstack_auth_shib.notifications import notifyManagers
 
 from django.utils.translation import ugettext_lazy as _
 
@@ -23,8 +26,7 @@ class ProjectRequestForm(forms.SelfHandlingForm):
     prjaction = forms.ChoiceField(
         label=_('Project action'),
         choices = [
-            ('newprj', _('Create personal project')),
-            ('selprj', _('Select existing projects'))
+            ('newprj', _('Create new project')),
         ],
         widget=forms.Select(attrs={
             'class': 'switchable',
@@ -86,12 +88,23 @@ class ProjectRequestForm(forms.SelfHandlingForm):
             pitem.name for pitem in self.request.user.authorized_tenants
         ]
 
-        prj_list = Project.objects.exclude(projectname__in=auth_prjs)
+        pendPReq = PrjRequest.objects.filter(registration__userid=self.request.user.id)
+        self.pendingProjects = [ prjreq.project.projectname for prjreq in pendPReq ]
+        excl_prjs = auth_prjs + self.pendingProjects
+
+        prj_list = Project.objects.exclude(projectname__in=excl_prjs)
         prj_list = prj_list.filter(status=PRJ_PUBLIC, projectid__isnull=False)
 
-        self.fields['selprj'].choices = [
+        prjEntries = [
             (prj_entry.projectname, prj_entry.projectname) for prj_entry in prj_list
         ]
+        if len(prjEntries):
+            self.fields['selprj'].choices = prjEntries
+            self.fields['prjaction'].choices = [
+                ('newprj', _('Create new project')),
+                ('selprj', _('Select existing projects'))
+            ]
+        
 
     @sensitive_variables('data')
     def handle(self, request, data):
@@ -115,6 +128,7 @@ class ProjectRequestForm(forms.SelfHandlingForm):
                 ))
 
         
+            prjliststr = ''
             for prjitem in prjlist:
         
                 if prjitem[3]:
@@ -132,6 +146,8 @@ class ProjectRequestForm(forms.SelfHandlingForm):
                         LOG.error("Cannot create project %s" % prjitem[0])
                         return False
                 
+                elif prjitem[0] in self.pendingProjects:
+                    continue
                 else:
                     project = Project.objects.get(projectname=prjitem[0])
                         
@@ -142,7 +158,18 @@ class ProjectRequestForm(forms.SelfHandlingForm):
                 }                
                 reqPrj = PrjRequest(**reqArgs)
                 reqPrj.save()
+                prjliststr += project.projectname + '\n'
 
+        
+            #
+            # TODO customize notification
+            #
+            if len(prjliststr):
+                msgSbj = _("Project subscription request")
+                msgBody = _("New subscription request from %s for the following projects:\n %s") \
+                        % (request.user.username, prjliststr)
+                notifyManagers(NotificationMessage(subject=msgSbj, body=msgBody))
+        
         return True
 
 
