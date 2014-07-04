5a8167f16f12b513730e26796b8a4bf431c43dda
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2014-07-04 08:41:43 +0200

    Changed age

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.command.bdist_rpm import bdist_rpm as _bdist_rpm
 
 pkg_name = 'openstack-security-integrations'
 pkg_version = '1.0.0'
-pkg_release = '8'
+pkg_release = '9'
 
 source_items = "setup.py src config"
 
