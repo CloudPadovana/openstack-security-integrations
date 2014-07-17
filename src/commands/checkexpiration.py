ca087078eb803db87bb2ef7591cc609e19488f88
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2014-07-17 10:55:27 +0200

    log support for checkexpiration

--- src/commands/checkexpiration.py
+++ src/commands/checkexpiration.py
@@ -1,4 +1,6 @@
 import sys
+import logging
+import logging.config
 
 from datetime import datetime
 from optparse import make_option
@@ -7,6 +9,8 @@ from django.core.management.base import BaseCommand, CommandError
 from openstack_auth_shib.models import Registration
 
 from keystoneclient.v3 import client
+
+LOG = logging.getLogger("checkexpiration")
 
 class Command(BaseCommand):
 
@@ -17,49 +21,67 @@ class Command(BaseCommand):
             default=None,
             help='The configuration file for this plugin'
         ),
+        make_option('--logconf',
+            dest='logconffile',
+            action='store',
+            default=None,
+            help='The configuration file for the loggging system'
+        ),
     )
     
     def readParameters(self, conffile):
         result = dict()
 
-        cfile = open(conffile)
-        for line in cfile:
-            tmps = line.strip()
-            if len(tmps) == 0 or tmps.startswith('#'):
-                continue
+        cfile = None
+        try:
+            cfile = open(conffile)
+            for line in cfile:
+                tmps = line.strip()
+                if len(tmps) == 0 or tmps.startswith('#'):
+                    continue
             
-            tmpl = tmps.split('=')
-            if len(tmpl) == 2:
-                result[tmpl[0].strip()] = tmpl[1].strip()
+                tmpl = tmps.split('=')
+                if len(tmpl) == 2:
+                    result[tmpl[0].strip()] = tmpl[1].strip()
+        except:
+            LOG.error("Cannot parse configuration file", exc_info=True)
         
-        cfile.close()
+        if cfile:
+            cfile.close()
         
         return result
     
     def handle(self, *args, **options):
     
+        logconffile = options.get('logconffile', None)
+        if logconffile:
+            logging.config.fileConfig(logconffile)
+        
         conffile = options.get('conffile', None)
         if not conffile:
+            logging.error("Missing configuration file")
             raise CommandError("Missing configuration file\n")
-    
+            
         try:
             params = self.readParameters(conffile)
+            
+            LOG.info("Checking expiration dates")
             
             expired_regs = Registration.objects.filter(expdate__lt=datetime.now())
             
             for reg_item in expired_regs:
                 
-                keystone = client.Client(username=params['username'],
-                                         password=params['passwd'],
-                                         project_name=params['tenantname'],
-                                         cacert=params['cafile'],
-                                         auth_url=params['authurl'])
+                keystone = client.Client(username=params['USERNAME'],
+                                         password=params['PASSWD'],
+                                         project_name=params['TENANTNAME'],
+                                         cacert=params['CAFILE'],
+                                         auth_url=params['AUTHURL'])
                 
                 keystone.users.update(reg_item.userid, enabled=False)
+                LOG.info("Disabled user %s" % reg_item.username)
                 
         except:
-            etype, evalue, etraceback = sys.exc_info()
-            sys.excepthook(etype, evalue, etraceback)
+            LOG.error("Check expiration failed", exc_info=True)
             raise CommandError("Check expiration failed")
 
 
