11b7e8ccebc4e71cf2a7ed72ec3eb5cb32d5a3fd
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2016-01-14 11:36:05 +0100

    Changed build number

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from distutils.command.bdist_rpm import bdist_rpm as _bdist_rpm
 
 pkg_name = 'openstack-security-integrations'
 pkg_version = '1.1.0'
-pkg_release = '3'
+pkg_release = '4'
 
 source_items = "setup.py src config"
 
