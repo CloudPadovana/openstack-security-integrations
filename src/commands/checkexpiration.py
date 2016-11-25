c89d2cfe40faba020abffeae17307bd33b608410
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2016-11-25 15:23:29 +0100

    Cron script for expired users removal

--- src/commands/checkexpiration.py
+++ src/commands/checkexpiration.py
@@ -16,12 +16,15 @@
 import logging
 import logging.config
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from optparse import make_option
 
+from django.conf import settings
 from django.db import transaction
 from django.core.management.base import BaseCommand, CommandError
 from openstack_auth_shib.models import Registration
+from openstack_auth_shib.models import Expiration
+from openstack_auth_shib.models import PrjRequest
 
 from keystoneclient.v3 import client
 
@@ -66,6 +69,17 @@ class Command(BaseCommand):
         
         return result
     
+    #
+    # TODO move in a common module
+    #
+    def get_prjman_roleid(self, keystone):
+        role_name = getattr(settings, 'TENANTADMIN_ROLE', 'project_manager')
+        
+        for tmp_role in keystone.roles.list():
+            if tmp_role.name == role_name:
+                return tmp_role.id
+        raise CommandError("Cannot retrieve project manager role id")
+    
     def handle(self, *args, **options):
     
         logconffile = options.get('logconffile', None)
@@ -77,43 +91,88 @@ class Command(BaseCommand):
             logging.error("Missing configuration file")
             raise CommandError("Missing configuration file\n")
             
+        params = self.readParameters(conffile)
+        
+        # Empty conf file used in rpm
+        if len(params) == 0:
+            return
+
+        LOG.info("Checking expired users")
         try:
-            params = self.readParameters(conffile)
-            
-            # Empty conf file used in rpm
-            if len(params) == 0:
-                return
-            
-            dtime = timedelta(int(params.get('DEFER_DAYS', '30')))
+
+            keystone_client = client.Client(username=params['USERNAME'],
+                                            password=params['PASSWD'],
+                                            project_name=params['TENANTNAME'],
+                                            cacert=params.get('CAFILE',''),
+                                            auth_url=params['AUTHURL'])
+
+            dtime = timedelta(int(params.get('DEFER_DAYS', '0')))
             exp_date = datetime.now() - dtime
-            LOG.info("Checking expiration dates before %s" % str(exp_date))
-            
-            users_expired = list()
-            with transaction.atomic():
-                expired_regs = Registration.objects.filter(expdate__lt=exp_date)
-                
-                for reg_item in expired_regs:
-                    LOG.info("Deleting user %s" % reg_item.username)
-                    users_expired.append((reg_item.username, reg_item.userid))
-
-                expired_regs.delete()
-
-            #
-            # TODO check for project admin role
-            #
-            
-            for user_name, user_id in users_expired:   
-                keystone = client.Client(username=params['USERNAME'],
-                                         password=params['PASSWD'],
-                                         project_name=params['TENANTNAME'],
-                                         cacert=params.get('CAFILE',''),
-                                         auth_url=params['AUTHURL'])
-                
-                keystone.users.delete(user_id)
-                LOG.info("Deleted user %s" % user_name)
+            exp_members = Expiration.objects.filter(expdate__lt=exp_date)
 
         except:
             LOG.error("Check expiration failed", exc_info=True)
             raise CommandError("Check expiration failed")
 
+        updated_prjs = set()
+
+        for mem_item in exp_members:
+
+            updated_prjs.add(mem_item.project.projectid)
+            
+            LOG.debug("Expired %s for %s" % \
+                    (mem_item.registration.username, mem_item.project.projectname))
+            
+            try:
+                with transaction.atomic():
+                    
+                    q_args = {
+                        'registration' : mem_item.registration,
+                        'project' : mem_item.project
+                    }
+                    Expiration.objects.filter(**q_args).delete()
+                    PrjRequest.objects.filter(**q_args).delete()
+
+                    arg_dict = {
+                        'project' : mem_item.project.projectid,
+                        'user' : mem_item.registration.userid
+                    }
+                    for r_item in keystone_client.role_assignments.list(**arg_dict):
+                        keystone_client.roles.revoke(r_item.role['id'], **arg_dict)
+                    
+                    LOG.info("Removed %s from %s" %
+                        (mem_item.registration.username, mem_item.project.projectid))
+
+                #
+                # TODO missing notification
+                #                    
+
+            except:
+                LOG.error("Check expiration failed for %s" % mem_item.registration.username,
+                            exc_info=True)
+
+        #
+        # Check for tenants without admin (use cloud admin if missing)
+        #
+        
+        prjman_roleid = self.get_prjman_roleid(keystone_client)
+        cloud_adminid = keystone_client.auth_ref.user_id
+
+        for prj_id in updated_prjs:
+        
+            try:
+                url = '/role_assignments?scope.project.id=%s&role.id=%s'
+                resp, body = keystone_client.get(url % (prj_id, prjman_roleid))
+                
+                if len(body['role_assignments']) == 0:
+                    keystone_client.roles.grant(prjman_roleid,
+                        user = cloud_adminid,
+                        project = prj_id
+                    )
+                    LOG.info("Cloud Administrator as admin for %s" % prj_id)
+            except:
+                #
+                # TODO notify error to cloud admin
+                #
+                LOG.error("No tenant admin for %s" % prj_id, exc_info=True)
 
