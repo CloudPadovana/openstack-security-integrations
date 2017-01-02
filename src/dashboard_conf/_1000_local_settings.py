75657da8d94b92ecae16d14e29f5755c3b5c6300
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2017-01-02 14:08:39 +0100

    Used snippet for configuration

---
+++ src/dashboard_conf/_1000_local_settings.py
@@ -0,0 +1,27 @@
+HORIZON_CONFIG['user_home'] = 'openstack_auth_shib.utils.get_user_home'
+
+AUTHENTICATION_PLUGINS = [
+    'openstack_auth_shib.backend.SKeyPluginFactory',
+    'openstack_auth.plugin.password.PasswordPlugin',
+    'openstack_auth.plugin.token.TokenPlugin'
+]
+
+AUTHENTICATION_URLS = ['openstack_auth_shib.urls']
+
+INSTALLED_APPS.append('openstack_auth_shib')
+
+NOTIFICATION_TEMPLATE_DIR = '/etc/openstack-auth-shib/notifications'
+
+#KEYSTONE_SECRET_KEY = ""
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE' : 'django.db.backends.mysql',
+#        'NAME' : '',
+#        'USER' : '',
+#        'PASSWORD' : '',
+#        'HOST' : '',
+#        'PORT' : '3306'
+#    }
+#}
+
