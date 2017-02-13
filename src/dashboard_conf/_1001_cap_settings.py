b78860b44b491088a609f506de06e7e36117dcc5
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2017-02-13 14:45:28 +0100

    Configlet for CAP and CEDC

---
+++ src/dashboard_conf/_1001_cap_settings.py
@@ -0,0 +1,2 @@
+HORIZON_CONFIG['help_url'] = 'http://www.pd.infn.it/cloud/Users_Guide/html-desktop/'
+
---
+++ src/dashboard_conf/_1001_cedc_settings.py
@@ -0,0 +1,2 @@
+HORIZON_CONFIG['help_url'] = 'https://cloud.cedc.csia.unipd.it/User_Guide/index.html'
+
