e1983478162ec547748a685587f0cdfc11d7c129
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2017-07-11 15:41:20 +0200

    Revised dashboard configlet

--- src/dashboard_conf/_1000_local_settings.py
+++ src/dashboard_conf/_1000_local_settings.py
@@ -1,11 +1,5 @@
 HORIZON_CONFIG['user_home'] = 'openstack_auth_shib.utils.get_user_home'
 
-AUTHENTICATION_PLUGINS = [
-    'openstack_auth_shib.backend.SKeyPluginFactory',
-    'openstack_auth.plugin.password.PasswordPlugin',
-    'openstack_auth.plugin.token.TokenPlugin'
-]
-
 AUTHENTICATION_URLS = ['openstack_auth_shib.urls']
 
 INSTALLED_APPS.append('openstack_auth_shib')
@@ -13,8 +7,6 @@ INSTALLED_APPS.append('openstack_auth_shib')
 NOTIFICATION_TEMPLATE_DIR = '/etc/openstack-auth-shib/notifications'
 
 HORIZON_CONFIG['identity_providers'] = []
-
-#KEYSTONE_SECRET_KEY = ""
 
 #DATABASES = {
 #    'default': {
