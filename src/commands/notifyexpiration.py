be23725f6bcb0fd42e9ebc3138f4bce61a843e55
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2017-11-07 15:54:13 +0100

    Fix for PDCL-869, first step

--- src/commands/notifyexpiration.py
+++ src/commands/notifyexpiration.py
@@ -21,6 +21,8 @@ from datetime import timedelta
 
 from django.core.management.base import BaseCommand, CommandError
 from openstack_auth_shib.models import Expiration
+from openstack_auth_shib.models import EMail
+from openstack_auth_shib.models import PrjRole
 from openstack_auth_shib.notifications import notification_render
 from openstack_auth_shib.notifications import notify as notifyUsers
 from openstack_auth_shib.notifications import USER_EXP_TYPE
@@ -28,9 +30,6 @@ from openstack_auth_shib.notifications import USER_EXP_TYPE
 from horizon.management.commands.cronscript_utils import build_option_list
 from horizon.management.commands.cronscript_utils import configure_log
 from horizon.management.commands.cronscript_utils import configure_app
-from horizon.management.commands.cronscript_utils import build_contact_list
-
-from keystoneclient.v3 import client
 
 LOG = logging.getLogger("notifyexpiration")
 
@@ -61,47 +60,62 @@ class Command(BaseCommand):
         config = configure_app(options)
                     
         try:
-            
+
             now = datetime.now()
-            contact_list = build_contact_list()
-            
+            noti_table = dict()
+            admin_table = dict()
+            user_set = set()
+
             for days_to_exp in self._get_days_to_exp(config.cron_plan):
-                
+
                 tframe = now + timedelta(days=days_to_exp)
-                
+                noti_table[days_to_exp] = list()
+
                 q_args = {
                     'expdate__gte' : tframe.replace(hour=0, minute=0, second=0, microsecond=0),
                     'expdate__lte' : tframe.replace(hour=23, minute=59, second=59, microsecond=999999)
                 }
-            
+
                 for exp_item in Expiration.objects.filter(**q_args):
+
+                    username = exp_item.registration.username
+                    userid = exp_item.registration.userid
+                    prjname = exp_item.project.projectname
+
+                    noti_table[days_to_exp].append((username, userid, prjname,))
+                    user_set.add(userid)
+
+                    if not admin_table.has_key(prjname):
+                        admin_table[prjname] = list()
+                        for p_role in PrjRole.objects.filter(project__projectname=prjname):
+                            admin_table[prjname].append(p_role.registration.userid)
+                            user_set.add(p_role.registration.userid)
+                        
+            mail_table = dict()
+            for email_item in EMail.objects.filter(registration__userid__in=user_set):
+                mail_table[email_item.registration.userid] = email_item.email
+
+            contact_table = dict()
+            for prjname, uid_list in admin_table.items():
+                contact_table[prjname] = list()
+                for userid in uid_list:
+                    if mail_table.has_key(userid):
+                        contact_table[prjname].append(mail_table[userid])
+
+            for days_to_exp, noti_list in noti_table.items():
+                for username, userid, prjname in noti_list:
                     try:
-                        
-                        keystone = client.Client(username=config.cron_user,
-                                                 password=config.cron_pwd,
-                                                 project_name=config.cron_prj,
-                                                 user_domain_name=config.cron_domain,
-                                                 project_domain_name=config.cron_domain,
-                                                 cacert=config.cron_ca,
-                                                 auth_url=config.cron_kurl)
-                        
-                        tmpuser = keystone.users.get(exp_item.registration.userid)
-                        
-                        
                         noti_params = {
-                            'username' : exp_item.registration.username,
+                            'username' : username,
+                            'project' : prjname,
                             'days' : days_to_exp,
-                            'contacts' : contact_list
+                            'contacts' : contact_table[prjname]
                         }
                         noti_sbj, noti_body = notification_render(USER_EXP_TYPE, noti_params)
-                        notifyUsers(tmpuser.email, noti_sbj, noti_body)
-                        
-                        #
-                        # TODO send notification to project admins
-                        #
-                        
+                        notifyUsers(mail_table[userid], noti_sbj, noti_body)
+                        LOG.info("Notified %s for %s" % (username, prjname))
                     except:
-                        LOG.warning("Cannot notify %s" % exp_item.registration.username, exc_info=True)
+                        LOG.error("Cannot notify %s" % username, exc_info=True)
                 
         except:
             LOG.error("Notification failed", exc_info=True)
