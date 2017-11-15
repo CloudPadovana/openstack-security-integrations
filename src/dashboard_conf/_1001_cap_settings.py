861f17e7353430a8d04b145f02bf9a8d66e041dc
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2017-11-15 14:31:13 +0100

    Unipd IdP for CAP

--- src/dashboard_conf/_1001_cap_settings.py
+++ src/dashboard_conf/_1001_cap_settings.py
@@ -14,7 +14,19 @@ HORIZON_CONFIG['identity_providers'].append(
     }
 )
 
+HORIZON_CONFIG['identity_providers'].append(
+    { 
+      'id' :          'unipd_sso',
+      'context' :     '/dashboard-unipd',
+      'path' :        '/dashboard-unipd/auth/register/',
+      'description' : 'UniPD IdP',
+      'logo' :        '/dashboard/static/dashboard/img/logoUniPD.png'
+    }
+)
+
 WEBSSO_IDP_MAPPING["infn_sso"] = ("infnaai", "mapped")
+WEBSSO_IDP_MAPPING["unipd_sso"] = ("unipdaai", "mapped")
+
 WEBSSO_IDP_ENTITIES["infnaai"] = [ "https://idp.infn.it/saml2/idp/metadata.php" ]
 WEBSSO_IDP_RULES["infn_sso"] = [
     {
@@ -32,5 +44,24 @@ WEBSSO_IDP_RULES["infn_sso"] = [
     }
 ]
 
-WEBSSO_CHOICES = WEBSSO_CHOICES + (('infn_sso', 'INFN AAI'),)
+
+WEBSSO_IDP_ENTITIES["unipdaai"] = [ "https://shibidp.cca.unipd.it/idp/shibboleth" ]
+WEBSSO_IDP_RULES["unipd_sso"] = [
+    {
+        "local": [
+            {
+                "user": {
+                    "name": "{0}",
+                    "domain": { "id": "default" },
+                    "type": "local"
+                }
+                
+            }
+        ],
+        "remote": [ { "type": "eppn" } ]
+    }
+]
+
+
+WEBSSO_CHOICES = WEBSSO_CHOICES + (('infn_sso', 'INFN AAI'), ('unipd_sso', 'UniPD IdP'),)
 
