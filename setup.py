f1143c7b83faa1df68ea050870e4866f07ca733f
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2017-11-20 10:16:47 +0100

    Minor changes for new CAP site

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from distutils.command.bdist_rpm import bdist_rpm as _bdist_rpm
 
 pkg_name = 'openstack-security-integrations'
 pkg_version = '1.2.0'
-pkg_release = '6'
+pkg_release = '7'
 
 source_items = "setup.py src config"
 
