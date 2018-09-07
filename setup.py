70e66687b9fe2e8cfc982795aebbc501954069e8
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2018-09-07 09:01:23 +0200

    Changed version and changelog

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from distutils.core import setup
 from distutils.command.bdist_rpm import bdist_rpm as _bdist_rpm
 
 pkg_name = 'openstack-security-integrations'
-pkg_version = '1.2.1'
+pkg_version = '1.2.2'
 pkg_release = '1'
 
 source_items = "setup.py src config"
