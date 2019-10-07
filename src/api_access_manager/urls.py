65011b339cc601a640591c7aea32cf7f627bbb48
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2019-10-07 14:39:36 +0200

    Skeleton files for PDCL-1404

---
+++ src/api_access_manager/urls.py
@@ -0,0 +1,33 @@
+#  Copyright (c) 2014 INFN - "Istituto Nazionale di Fisica Nucleare" - Italy
+#  All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License. 
+
+from django.conf.urls import url
+
+from openstack_dashboard.dashboards.project.api_access import views as baseViews
+from openstack_dashboard.dashboards.project.api_access_manager import views
+
+urlpatterns = [
+    url(r'^$', baseViews.IndexView.as_view(), name='index'),
+    url(r'^ec2/$', baseViews.download_ec2_bundle, name='ec2'),
+    url(r'^clouds.yaml/$',
+        baseViews.download_clouds_yaml_file, name='clouds.yaml'),
+    url(r'^openrc/$', views.download_rc_file, name='openrc'),
+    url(r'^openrcv2/$', baseViews.download_rc_file_v2, name='openrcv2'),
+    url(r'^view_credentials/$', baseViews.CredentialsView.as_view(),
+        name='view_credentials'),
+    url(r'^recreate_ec2_credentials/$',
+        baseViews.RecreateCredentialsView.as_view(), name='recreate_credentials'),
+]
+
---
+++ src/api_access_manager/views.py
@@ -0,0 +1,48 @@
+#  Copyright (c) 2014 INFN - "Istituto Nazionale di Fisica Nucleare" - Italy
+#  All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License. 
+
+import logging
+
+from django.conf import settings
+
+from openstack_dashboard.dashboards.project.api_access import views as baseViews
+from openstack_auth_shib.idpmanager import Federated_Account
+
+LOG = logging.getLogger(__name__)
+
+def download_rc_file(request):
+
+    f_account = Federated_Account(request)
+    if not f_account:
+        return baseViews.download_rc_file(request)
+
+    template = getattr(settings, 'OPENRC_CUSTOM_TEMPLATE',
+                       'project/api_access_manager/openrc.sh.template')
+
+    context = baseViews._get_openrc_credentials(request)
+    context['user_domain_name'] = request.user.user_domain_name
+    try:
+        project_domain_id = request.user.token.project['domain_id']
+    except KeyError:
+        project_domain_id = ''
+    context['project_domain_id'] = project_domain_id
+    context['os_identity_api_version'] = 3
+    context['os_auth_version'] = 3
+
+    #TODO retrieve token
+    context['os_auth_token'] = ""
+
+    return baseViews._download_rc_file_for_template(request, context, template)
+
